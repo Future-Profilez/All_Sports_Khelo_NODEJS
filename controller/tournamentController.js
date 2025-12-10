989db9b3cb414a9f0db748dc517f32ef6dde91ee
merndevfp01-ctrl <mern.dev.fp01@gmail.com>
2025-12-10 16:59:26 +0530

    Update

--- controller/tournamentController.js
+++ controller/tournamentController.js
@@ -5,19 +5,32 @@ const fetchTournament = async (req, res) => {
     try {
         const tournament = await prisma.tournaments.findMany();
         const tournaments = convertBigIntToString(tournament);
-        
-        return res.status(200).json({
-            message: "Tournaments fetched successfully!",
-            data : tournaments
-        });
-
+        return res.status(200).json({ message: "Tournaments fetched successfully!", data: tournaments });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({
-            message: "Internal server error",
-            error
-        });
+        return res.status(500).json({ message: "Internal server error", error });
     }
 }
 
-module.exports = { fetchTournament }
+const fetchContent = async (req, res) => {
+    try {
+        const slug = req.params.slug;
+        const data = await prisma.tournaments.findFirst({
+            where: { slug_name: slug },
+            include: {
+                contents: true,
+            },
+        });
+        if (!data) {
+            return res.status(404).json({ message: "Tournament not found", });
+        }
+        const content = convertBigIntToString(data);
+        return res.status(200).json({ message: "Tournament content fetched successfully!", content: content, });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error", error });
+    }
+};
+
+
+module.exports = { fetchTournament, fetchContent }
