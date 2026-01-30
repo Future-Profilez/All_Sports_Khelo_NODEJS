69bc97666221e6a5901bbbdade485bba2c046ee8
naveenfutureprofilez <naveen@internetbusinesssolutionsindia.com>
2026-01-30 11:14:41 +0530

    merge

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -253,7 +253,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma/schema",
--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -254,7 +254,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma/schema",
