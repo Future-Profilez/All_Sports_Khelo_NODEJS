4b81fce17fd1107f1cb144b1679a1bfb442cb1bc
shivanijwork <shivanijwork@gmail.com>
2026-02-21 13:04:39 +0530

    mergrations changes

--- prisma/sql/202601201044_add_new_column_organizer_name_in_tournaments_table.sql
+++ prisma/sql/202601201044_add_new_column_organizer_name_in_tournaments_table.sql
@@ -1,3 +1,1 @@
- ALTER TABLE `ask_tournaments` ADD COLUMN `organizer_name` VARCHAR(255) NULL AFTER `slug_name`,
-MODIFY `user_id` INTEGER NOT NULL,
-MODIFY `state_id` INTEGER NOT NULL;
+ALTER TABLE `ask_tournaments` MODIFY `user_id` INTEGER NOT NULL, MODIFY `state_id` INTEGER NOT NULL;
--- prisma/sql/202602171515_add_null_value_to_ask_tournaments_in_country_city_and_state.sql
+++ prisma/sql/202602171515_add_null_value_to_ask_tournaments_in_country_city_and_state.sql
@@ -1,5 +1,5 @@
-ALTER TABLE `ask_tournaments` MODIFY `sport_id` CHAR(36) NOT NULL,
-    MODIFY `description` VARCHAR(255) NULL,
+ALTER TABLE `ask_tournaments` MODIFY `sport_id` VARCHAR(100) NOT NULL,
+    MODIFY `description` TEXT NULL,
     MODIFY `country_id` BIGINT UNSIGNED NULL,
     MODIFY `state_id` BIGINT UNSIGNED NULL,
     MODIFY `city_id` BIGINT UNSIGNED NULL;
