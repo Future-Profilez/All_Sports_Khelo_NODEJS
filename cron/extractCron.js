e718d959417db4794e17f0507eaba53f0e2e93a0
shivanijwork <shivanijwork@gmail.com>
2026-03-16 14:50:14 +0530

    cron job

--- cron/extractCron.js
+++ cron/extractCron.js
@@ -1,5 +1,8 @@
 const cron = require("node-cron");
 
+async function extractConsole({ sport }) {
+        console.log(`{${sport} tournament extractions finished.}`);
+}
 const {
     extractChessTournaments,
     extractTableTennisTournament,
@@ -14,17 +17,28 @@ cron.schedule("0 */12 * * *", async () => {
     // console.log("Running tournament extraction cron...");
     console.log("⏰ Cron triggered at:", new Date().toISOString());
     try {
-        // await Promise.all([
-        //     extractChessTournaments(),
-        //     extractTableTennisTournament(),
-        //     extractHandballTournament(),
-        //     extractpickleballTournament(),
-        //     extractSquashTournament(),
-        //     extractbasketballTournament(),
-        // ]);
-         await extractAllTournaments();
-        console.log("Tournament extraction completed");
+
+        await extractChessTournaments();
+        await extractConsole("Chess");
+
+        await extractTableTennisTournament();
+        await extractConsole("Table Tennis");
+
+        await extractHandballTournament();
+        await extractConsole("Handball");
+
+        await extractpickleballTournament();
+        await extractConsole("Pickleball");
+
+        await extractSquashTournament();
+        await extractConsole("Squash");
+
+        await extractbasketballTournament();
+        await extractConsole("Basketball");
+
+        console.log("🎉 All tournament extractions completed");
+
     } catch (error) {
-    console.log("Cron extraction failed:", error.message);
-}
+        console.log("Cron extraction failed:", error.message);
+    }
 });
